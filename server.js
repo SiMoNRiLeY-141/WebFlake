a22af0a414e73beb89c4867ed2ab04458c64ac40
Adhil Jahan <adhiljahan2@gmail.com>
2024-05-27 17:41:03 +0530

    Format

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.use(limiter);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
